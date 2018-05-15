a70540abf964c9465188410398e9ffbe849bd5b4
Larry Finn <larry@actioniq.co>
2018-05-15 17:50:43 -0400

    cleaning

--- src/scala/co/actioniq/slick/logging/TransactionAction.scala
+++
@@ -1,8 +0,0 @@
-package co.actioniq.slick.logging
-
-object TransactionAction extends Enumeration {
-  type TransactionAction = Value
-  val create = Value("create")
-  val update = Value("update")
-  val delete = Value("delete")
-}
